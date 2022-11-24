7e48e63d18fe13546c989b12b4fafdda8c277911
deocleciano <deocleciano001@gmail.comgit config --global user.email deocleciano001@gmail.comgit config --global user.email deocleciano001@gmail.com>
2022-11-24 19:45:45 -0300

    Adição dos componentes Button

--- src/main/Calculator .jsx
+++ src/main/Calculator .jsx
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import './Calculator.css'
+import Button from "../components/Button";
 
 export default class Calculator extends Component {
     render(){
         return(
             <div className="calculator">
-                <h1>calculator</h1>
+                <Button lable="AC"></Button>
+                <Button lable="/"></Button>
+                <Button lable="7"></Button>
+                <Button lable="8"></Button>
+                <Button lable="9"></Button>
+                <Button lable="*"></Button>
+                <Button lable="4"></Button>
+                <Button lable="5"></Button>
+                <Button lable="6"></Button>
+                <Button lable="-"></Button>
+                <Button lable="1"></Button>
+                <Button lable="2"></Button>
+                <Button lable="3"></Button>
+                <Button lable="+"></Button>
+                <Button lable="0"></Button>
+                <Button lable="."></Button>
+                <Button lable="="></Button>
             </div>
         )
     }
